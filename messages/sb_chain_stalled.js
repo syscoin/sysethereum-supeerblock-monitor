b3c8d56cf48c97f2517ce4538cb2f0f245576f6e
Dan Wasyluk <987186+dwasyluk@users.noreply.github.com>
2020-01-14 14:53:12 -0500

    better email subject

--- messages/sb_chain_stalled.js
+++ messages/sb_chain_stalled.js
@@ -1,5 +1,5 @@
 module.exports = {
-  subject: `ACTION REQUIRED: ETH SB chain has stalled`,
+  subject: `ACTION REQUIRED: No events on superblock contract since {{remote}}`,
   text: `The ETH SB chain appears to have stalled. Action may be required!\n Local height: {{local}} \n Last SB TX height: {{remote}}`,
   html: `<p>The ETH SB chain appears to have stalled. Action may be required!</p>
          <p><b>Local height:</b> {{local}}</p>
