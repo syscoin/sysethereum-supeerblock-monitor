5a97a38cc138cf5b118fc1c6f7eb0a7316b46517
Dan Wasyluk <987186+dwasyluk@users.noreply.github.com>
2020-02-14 23:54:47 -0500

    restore passive status checking

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = require('express')();
-const cors = require('cors')
+const cors = require('cors');
 const nodemailer = require('nodemailer');
 
 const config = require('./config');
@@ -7,15 +7,22 @@ const utils = require('./utils');
 
 let mailConfig = utils.configMailer(config);
 let transporter = nodemailer.createTransport(mailConfig);
+let checkInterval;
 
-app.use(cors())
+async function checkForAlerts(mailer) {
+  return await utils.checkEthereumSuperblockContract(mailer);
+}
+
+// start passively checking for uptime
+checkInterval = setInterval(checkForAlerts, config.interval * 1000, transporter);
+
+// enable a webserver for proactive status checks
+app.use(cors());
 
 app.get('/status', async (req, res) => {
-  const status = await utils.checkEthereumSuperblockContract(transporter);
-
+  const status = await checkForAlerts(transporter);
   return res.send({ ...status });
 });
 
 app.listen(config.port);
-
-console.log('Sysethereum sb monitor started.');
+console.log(`Sysethereum sb monitor started on port ${config.port}`);
